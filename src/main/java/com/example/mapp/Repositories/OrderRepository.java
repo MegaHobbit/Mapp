bfcc521541aa903d7701cc454c36eb41575b0326
Megahobbit <22wonderfulmurimi@gmail.com>
2026-06-30 10:52:22 +0300

    Removed project modules from tutorials that don't align to a HMIS

--- src/main/java/com/example/mapp/Repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mapp.Repositories;
-
-import com.example.mapp.Models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
